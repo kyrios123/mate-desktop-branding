d3cb169050c21e3d5bbc5342044a48bdb60bab32
Pierre-Yves <pyu@riseup.net>
2020-04-28 21:20:37 +0200

    Update 60-mate-accessibility.sh

--- env/60-mate-accessibility.sh
+++ env/60-mate-accessibility.sh
@@ -1,7 +1,7 @@
 # Begin /usr/share/defaults/etc/profile.d/60-mate-accessibility.sh
 
 if [ -z "${GTK_MODULES}" ]; then
-    export GTK_MODULES="gail:atk-bridge"
+    export GTK_MODULES="gail:atk-bridge:canberra-gtk-module"
 fi
 
 # End /usr/share/defaults/etc/profile.d/60-mate-accessibility.sh
